6e007b87cd8e7a97bfa9f7bb74e942c16e6480fb
David Stone <david@nnucomputerwhiz.com>
2026-08-19 01:25:15 -0600

    chore: mark tested up to WordPress 7.1

--- gp-translate-with-openai.php
+++ gp-translate-with-openai.php
@@ -7,6 +7,7 @@
  * Tags:              glotpress, translate, machine translate, openai, chatgpt
  *
  * Requires at least: 4.9
+ * Tested up to:     7.1
  * Requires PHP:      7.4
  * Version:           1.1
  *
